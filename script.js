dc41c70945123dfdcddca83ef805e4486bfc2863
jjhernandez06 <jherz1997@gmail.com>
2024-09-20 23:48:30 -0600

    Agregando

---
+++ script.js
@@ -0,0 +1,5 @@
+function showLoveMessage() {
+    const message = document.getElementById("message");
+    message.innerText = "¡Por ser buena nota y porque sé que le gustan. Quizás el detalle no sea físico, pero sí creo que algo único! 🌻";
+}
+
